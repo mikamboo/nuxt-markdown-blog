2410a55698d5b762a72f3f3a62b9d05d90682c7e
Michael <mike@mikangali.com>
2019-05-19 23:05:28 +0200

    Etape 4: Création vue dynamique des articles

---
+++ pages/posts/_slug.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :key="$route.params.slug">
+    <section class="hero is-medium is-dark is-bold" :style="{ background: 'url('+ attributes.cover +')' }">
+      <div class="hero-body">
+        <h1 class="title is-size-2">
+          {{ attributes.title }}
+        </h1>
+        <h2 v-if="attributes.date" class="subtitle">
+          {{ new Date(attributes.date).toLocaleDateString() }}
+        </h2>
+      </div>
+    </section>
+    <br>
+    <div class="container">
+      <div class="content has-text-justified" v-html="content"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ params }) {
+    const res = await import(`~/content/posts/${params.slug}.md`)
+    return {
+      attributes: res.attributes,
+      content: res.html
+    }
+  }
+}
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <div class="content">
+      <h1 class="title is-size-2">
+        Articles
+      </h1>
+      <ul>
+        <li>
+          <nuxt-link to="/posts/article-1">Article 1</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/posts/article-1">Article 2</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
